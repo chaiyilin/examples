af986cd580d5a9ab88dea4c27c64d44ac22b9076
yilin chai <chaiyilin@hotmail.com>
2018-04-01 21:50:08 +1000

    upgrade

--- src/stories/camelcase.js
+++ src/stories/camelcase.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Camelcase } from '../components/camelcase';
 
+console.log(Camelcase);
 export const stories = storiesOf('01-camelcase', module).add('example', () => (
   <Camelcase />
 ));
